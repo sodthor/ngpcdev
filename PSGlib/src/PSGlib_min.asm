b89cc093a3810e8aba3eb5fdaf601839f6adb382
sodthor <37836242+sodthor@users.noreply.github.com>
2026-04-01 19:18:00 +0200

    140b

--- PSGlib/src/PSGlib_min.asm
+++ PSGlib/src/PSGlib_min.asm
@@ -27,80 +27,31 @@ PSGSubString   EQU 008h
 ; d': substring length
 ; e': loop flag
 ;hl': music ptr
-; ix: PSGMusicLoopPoint
-; iy: PSGMusicSubstringRetAddr
 
 _start_z80:
   di
-  ld sp, _stack ; stack: max 6 bytes
-  jr _init
-_stack:
+  xor a
+  jr PSGStop
+  nop
+  nop
+_stack: ; 6 bytes needed: loop, substring and rst
 
 _noFrameSkip:
   exx
-  rst 20h ; jr _intLoop
+  rst 28h ; jr _intLoop
 
 ;PSGStop_: called by rst 08h
+  ; 9 bytes
   ld (hl),PSGLatch|PSGChannel0|PSGVolumeData|00Fh   ; latch channel 0, volume=0xF (silent)
   ld (hl),PSGLatch|PSGChannel1|PSGVolumeData|00Fh   ; latch channel 1, volume=0xF (silent)
   ld (hl),PSGLatch|PSGChannel2|PSGVolumeData|00Fh   ; latch channel 2, volume=0xF (silent)
   ld (hl),PSGLatch|PSGChannel3|PSGVolumeData|00Fh   ; latch channel 3, volume=0xF (silent)
   ret
 
-_setLoopPoint:
-  push hl
-  pop ix
-  rst 20h ; jr _intLoop
-
-_dontLoop:
-  exx
-PSGStop:
-  ld hl,04000h
-  rst 08h
-  inc l
-  rst 08h
-_init:
-  ld d,PSG_STOPPED                         ; PSGMusicStatus in d: set status to PSG_STOPPED (0)
-  jr _mainLoop
-
-; called by rst 20h
-  pop bc ; remove return address from stack
-_intLoop:
-  ld a,(hl)                      ; load PSG byte directly into A!
-  inc hl                         ; point to next byte
-  dec d                          ; test d for 0
-  inc d                          ; restoring d, sets Z if d==0!
-  jr z,_continue                 ; check if it is 0 (we are not in a substring)
-  dec d                          ; decrease len
-  jr nz,_continue
-  push iy
-  pop hl                         ; substring is over, retrieve return address (PSGMusicSubstringRetAddr)
-
-_continue:
-  ld bc, 04000h
-  cp PSGLatch                    ; is it a latch?
-  jr c,_noLatch                  ; if < $80 then it is NOT a latch
-  
-  ; we have got the latch PSG byte both in A and in B
-  ; and we have to check if the value should pass to PSG or not
-  bit 4,a                        ; test if it is a volume
-  jr z,_noVolume                 ; jump if not volume data
-  ; set volume
-  ld (bc),a
-_send2PSG:
-  inc c                          ; bc = 04001h
-_send2PSG_:
-  ld (bc),a                      ; output the byte to 04000h or 04001h
-  rst 20h ; jr _intLoop
-
-_noVolume:
-  cp 0E0h
-  jr c,_send2PSG                 ; send data to PSG if it is for channels 0-1 or 2 (04001h)
-  jr _send2PSG_                  ; output the byte in noise register (04000h)
-
 _noLatch:
+  ; 23 bytes
   cp PSGData
-  jr nc,_send2PSG                ; if < $40 then it is a command -> send to 04001h
+  jr nc,_send2PSG                ; if < $40 then it is a command else send to 04001h
 ;_command:
   cp PSGWait
   jr z,_done                     ; no additional frames
@@ -112,11 +63,62 @@ _noLatch:
   jr nz,_setLoopPoint
   or e                           ; looping requested? (a is 0 here)
   jr z,_dontLoop                 ; No:stop it! (tail call optimization)
-  push ix
   pop hl
-  rst 20h ; jr _intLoop
+  push hl
+  rst 28h ; jr _intLoop
+
+; called by rst 28h
+  ; 34 bytes
+  pop bc ; remove return address from stack
+;_intLoop:
+  ld a,(hl)                      ; load PSG byte directly into A!
+  inc hl                         ; point to next byte
+  inc d                          ; test d for 0
+  dec d                          ; rstoring d, sets Z if d==0
+  jr z,_continue                 ; check if it is 0 (we are not in a substring)
+  dec d                          ; decrease len
+  jr nz,_continue
+  pop hl                         ; substring completed, get back music ptr from stack
+_continue:
+  ld bc, 04000h
+  cp PSGLatch                    ; is it a latch?
+  jr c,_noLatch                  ; if < $80 then it is NOT a latch
+  ; we have got the latch PSG byte both in A and in B
+  ; and we have to check if the value should pass to PSG or not
+  bit 4,a                        ; test if it is a volume
+  jr z,_noVolume                 ; jump if not volume data
+_setVolume
+  ld (bc),a
+_send2PSG:
+  inc c                          ; bc = 04001h
+_send2PSG_:
+  ld (bc),a                      ; output the byte to 04000h or 04001h
+  rst 28h ; jr _intLoop
+_noVolume:
+  cp 0E0h
+  jr c,_send2PSG                 ; send data to PSG if it is for channels 0-1 or 2 (04001h)
+  jr _send2PSG_                  ; output the byte in noise register (04000h)
+
+_setLoopPoint:
+  ; 3 bytes
+  pop bc
+  push hl
+  rst 28h
+
+_dontLoop:
+  ; 13 bytes
+  exx
+PSGStop:
+  ld sp, _stack ; to be sure stack is clean
+  ld hl,04000h
+  rst 08h
+  inc l
+  rst 08h
+  ld d,a                         ; PSGMusicStatus in d: set status to PSG_STOPPED (a is 0 here)
+  jr _mainLoop
 
 _runCommand: ; a is 3 (loop) or 2 (no loop) or 1 (stop)
+  ; 39 bytes
   dec a
   jr z,PSGStop
 ;PSGPlay:
@@ -125,7 +127,6 @@ _runCommand: ; a is 3 (loop) or 2 (no loop) or 1 (stop)
   ld e,a                          ; loop flag
   ld hl,_music_start_             ; music ptr in hl'
   push hl                         ; default loop pointer points to begin too
-  pop ix
   xor a
   ld d,a                          ; reset the substring len (for compression)
 _setFrameSkip:
@@ -136,7 +137,6 @@ _setFrameSkip:
   exx
 _done:
   exx
-
 _mainLoop:
   xor a
   ld hl, 08000h
@@ -148,25 +148,25 @@ _waitLoop:
   jr nz, _runCommand
 ;PSGFrame:
   or d               ; check if we have got to play a tune (a is 0 here)
-  jr z, _waitLoop    ; no music to play : return
-  and e              ; check if we have got to skip frames (a  is not 0 here)
+  jr z, _mainLoop    ; no music to play : return
+  and e              ; check if we have got to skip frames (a is not 0 here)
   jr z,_noFrameSkip
 ;_skipFrame:
   dec e
   jr _mainLoop
 
 _substring:
+  ; 13 bytes
   ld c,(hl)                      ; load substring address (offset)
   inc hl
   ld b,(hl)
   inc hl
   push hl
-  pop iy                         ; PSGMusicSubstringRetAddr
   ld hl,_music_start_
   add hl,bc                      ; make substring current
   sub PSGSubString-4             ; len is value - $08 + 4
   ld d,a                         ; save len
-  rst 20h ; jr _intLoop
+  rst 28h ; jr _intLoop
 
 _music_start_:
 
